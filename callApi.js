752873797ad4c6fc0bb9002c7dc41aa3d204e69b
Chay Auriane <chaya.07@protonmail.com>
2022-09-28 09:10:41 +0200

    feat: loop through each plugin's div

--- callApi.js
+++ callApi.js
@@ -1,10 +1,12 @@
 
-const divPlugin = document.querySelector('.cnalps-weather-widget');
-if (divPlugin) {
-    fetch('https://www.weatherwp.com/api/common/publicWeatherForLocation.php?city=' + divPlugin.getAttribute('data-city') + '&country=' + divPlugin.getAttribute('data-country') + '&language=french')
-        .then(response => response.json())
-        .then(response => {
-            let html = '<p class=\"weather-title\"> La météo à ' + response.status_message + ' :</p> <img src=\" ' + response.icon + '\"> <p>' + response.temp + ' °C</p><p>' + response.description + '</p>';
-            divPlugin.innerHTML = html;
-        });
-}
+const divsPlugin = document.querySelectorAll('.cnalps-weather-widget');
+divsPlugin.forEach(divPlugin => {
+    if (divPlugin) {
+        fetch('https://www.weatherwp.com/api/common/publicWeatherForLocation.php?city=' + divPlugin.getAttribute('data-city') + '&country=' + divPlugin.getAttribute('data-country') + '&language=french')
+            .then(response => response.json())
+            .then(response => {
+                let html = '<p class=\"weather-title\"> La météo à ' + response.status_message + ' :</p> <img src=\" ' + response.icon + '\"> <p>' + response.temp + ' °C</p><p>' + response.description + '</p>';
+                divPlugin.innerHTML = html;
+            });
+    }
+});
